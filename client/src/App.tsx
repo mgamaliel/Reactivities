7e26740977dfb5cd89310dd03f1dd602fddc869e
Martin Gamaliel Alvarez Gomez <alvarezgomez.mg@hotmail.com>
2020-12-01 22:36:32 -0800

    Fetch activities from api with axios

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,55 @@
-import React, { FC } from 'react'
+import axios from 'axios'
+import React, { FC, useEffect, useState } from 'react'
 import 'semantic-ui-css/semantic.css'
-import { Header, Icon } from 'semantic-ui-react'
+import Navbar from './components/Navbar'
+import { Activity } from './types'
+import './App.css'
+import { Button, Container, Grid, Item, Label, Segment } from 'semantic-ui-react'
 
-const App: FC = (): JSX.Element => (
-    <Header as="h2">
-        <Icon name="users" />
-        <Header.Content>Reactivities</Header.Content>
-    </Header>
-)
+const App: FC = (): JSX.Element => {
+    const [activities, setActivities] = useState<Activity[]>([])
+
+    useEffect(() => {
+        axios
+            .get<Activity[]>('/api/activities')
+            .then((response) => setActivities([...response.data]))
+    }, [])
+
+    return (
+        <>
+            <Navbar />
+            <Container style={{ marginTop: '8em' }}>
+                <Grid>
+                    <Grid.Column width={10}>
+                        <Segment clearing>
+                            <Item.Group divided>
+                                {activities.map((activity) => (
+                                    <Item key={activity.id}>
+                                        <Item.Content>
+                                            <Item.Header as="a">{activity.title}</Item.Header>
+                                            <Item.Meta>{activity.date}</Item.Meta>
+                                            <Item.Description>
+                                                <div>{activity.description}</div>
+                                                <div>
+                                                    {activity.city}, {activity.venue}
+                                                </div>
+                                            </Item.Description>
+                                            <Item.Extra>
+                                                <Button floated="right" color="blue">
+                                                    View
+                                                </Button>
+                                                <Label basic>{activity.category}</Label>
+                                            </Item.Extra>
+                                        </Item.Content>
+                                    </Item>
+                                ))}
+                            </Item.Group>
+                        </Segment>
+                    </Grid.Column>
+                </Grid>
+            </Container>
+        </>
+    )
+}
 
 export default App
---
+++ client/src/types/models.d.ts
@@ -0,0 +1,9 @@
+export type Activity = {
+    id: string
+    title: string
+    description: string
+    category: string
+    date: Date
+    city: string
+    venue: string
+}
